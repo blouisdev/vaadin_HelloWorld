8eba673b190bca89f2afebc02c26f5a5478781b1
LOUIS Bryan <louis.bryan.dev@outlook.com>
2022-06-02 21:56:26 +0200

    dev: my first test class

---
+++ src/test/java/com/example/application/MyTodoTest.java
@@ -0,0 +1,14 @@
+package com.example.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class MyTodoTest { 
+	
+	@Test
+	public void myFirstTest() {
+		assertThat(true).isTrue();
+	}
+
+}
